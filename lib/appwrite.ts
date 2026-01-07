4624590d502fd4e3d7bd5c2f836f85bb0417b720
Karl <karlcaparal05@gmail.com>
2026-01-08 00:03:23 +0800

    Update code changes

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.kc.foodordering",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseeId: "693a18ff003b0aeec6bf",
+    userCollectionId: "user"
+}; 
